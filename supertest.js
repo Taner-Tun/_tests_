4b0dc09f57a64d76da1814c8d97b95da1b50fdd5
Taner Tun <t_tun@msn.com>
2022-09-12 15:05:51 +0300

    update

--- supertest.js
+++ supertest.js
@@ -1,104 +1,38 @@
 const request = require("supertest")
 
-const HOST = process.env.HOST || "https://petstore.swagger.io"
+const HOST = process.env.HOST || "https://petstore.swagger.io/";
 
-afterAll(async () => { 
-	await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
+afterAll(async () => {
+  await new Promise(resolve => setTimeout(() => resolve(), 500)); //avoid just open handle
 });
 
-describe('When testing jest', () => {
-	describe('given i have a non failing test', () => {
-		it('should be one', () => {
-			expect(1).toBe(1)
-		})
-	})
+describe("Testing JEST", () => {
+  describe("given that jest is working", () => {
+    it("should work", () => {
+      expect(1).toBe(1);
+    });
+  });
+});
 
-	describe('given i have a non failing test', () => {
-		it('should be two', () => {
-			expect(2).toBe(2)
-		})
-	})
-})
-
-describe('When testing jest', () => {
-	describe('given i have a non failing test', () => {
-		it.skip('should fail', () => {
-			expect(1).toBe(2)
-		})
-	})
-})
-
-describe("Testing petshop", () => {
-	describe('given a broken url', () => {
-		it('should return status 404', () => {
-			const container = request(HOST)
-			container.get('/whatever')
-				.expect(404)
-		})
-	})
-})
+describe("Testing JEST", () => {
+  describe.skip("given that jest is working", () => {  //use skip to disable a test
+    it("should work", () => {
+      expect(1).toBe(1);
+    });
+  });
+});
 
 describe("Testing petshop with supertest", () => {
-	describe('given GET /', () => {
-		it('should return status 200', () => {
-			container = request(HOST)
-			container.get('/')
-				.expect('Allow', /GET/)
-				.expect(200)
-				.expect('Content-Type', 'text/html')
-		})
-	})
-})
+  describe("given GET /", () => {
+    it("should return 200", () => {
+      container = request(HOST)
+      container.get('/')
+        .expect(200)
+        .expect('Allow', /GET/)
+        .expect('Allow', /DELETE/)
+        .expect('Allow', /POST/)
+        .expect('Content-Type', 'application/json')
 
-describe("Testing petshop with supertest", () => {
-	describe('given POST /v2/pet', () => {
-		it('should return status 200', () => {
-			container = request(HOST)
-			container.post('/v2/pet')
-				.expect('Allow', /POST/)
-				.expect(200);
-		})
-	})
-})
-
-const pet = {
-	"category": {
-	  "name": "string"
-	},
-	"name": "doggie",
-	"photoUrls": [
-	  "string"
-	],
-	"tags": [
-	  {
-		"id": 0,
-		"name": "string"
-	  }
-	],
-	"status": "available"
-}
-
-describe("Testing petshop with supertest", () => {
-	describe('given POST /v2/pet', () => {
-		it('should add pet', () => {
-		container = request(HOST)
-		container
-			.post('/v2/pet')
-			.send(pet)
-			.set('Accept', 'application/json')
-			.expect(200,/id/, function(err, res) {
-				if (res.body.id.toString() < 19) {
-				  throw new Error('ID too short');
-				}
-			})
-			.expect(200,/name/)
-			.expect(200,/tags/)
-			.expect(200,/status/)
-			.expect('Content-Type', 'application/json')
-			.expect(function(err, res) {
-				if (err) return done(err);
-				done()
-			})
-		})
-	})
-})
+    });
+  });
+});
